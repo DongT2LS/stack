7ad4a8d4a3c413916f7ac7db5a9b37fc5605b10d
vanhein445566@gmail.com <vanhein445566@gmail.com>
2021-07-23 18:28:56 +0700

    thugithub

---
+++ stack.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    int n;
+    scanf("%d",&n);
+    stack<int> st;
+    int a;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        st.push(a);
+    }   
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+}
